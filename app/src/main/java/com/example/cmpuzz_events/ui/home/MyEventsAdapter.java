d714191b92d40d124b1f688502cd8d7b4e816df4
andyjianzhou <andyzhou727@gmail.com>
2025-11-06 01:34:34 -0700

    Using separate class for EventViewHolder

--- app/src/main/java/com/example/cmpuzz_events/ui/home/MyEventsAdapter.java
+++ app/src/main/java/com/example/cmpuzz_events/ui/home/MyEventsAdapter.java
@@ -3,7 +3,6 @@ package com.example.cmpuzz_events.ui.home;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,7 +12,7 @@ import com.example.cmpuzz_events.ui.event.Event;
 
 import java.util.List;
 
-public class MyEventsAdapter extends RecyclerView.Adapter<MyEventsAdapter.EventViewHolder> {
+public class MyEventsAdapter extends RecyclerView.Adapter<EventViewHolder> {
 
     private List<Event> events;
 
@@ -43,24 +42,5 @@ public class MyEventsAdapter extends RecyclerView.Adapter<MyEventsAdapter.EventV
     @Override
     public int getItemCount() {
         return events.size();
-    }
-
-    static class EventViewHolder extends RecyclerView.ViewHolder {
-        private final TextView tvEventName;
-        private final TextView tvDescription;
-        private final TextView tvMaxAttendees;
-
-        public EventViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tvEventName = itemView.findViewById(R.id.tvEventName);
-            tvDescription = itemView.findViewById(R.id.tvDescription);
-            tvMaxAttendees = itemView.findViewById(R.id.tvMaxAttendees);
-        }
-
-        public void bind(Event event) {
-            tvEventName.setText(event.getTitle());
-            tvDescription.setText(event.getDescription());
-            tvMaxAttendees.setText("Maximum Attendees: " + event.getCapacity());
-        }
     }
 }
